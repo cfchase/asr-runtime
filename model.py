ed4beee82aec970d293100d4de2cfa8c062d9479
Chris Chase <cchase@redhat.com>
2024-06-19 11:40:47 -0400

    Changed pipeline to use ASR Pipeline instead of diffusers

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import argparse
 
 from typing import Dict, Union
 import torch
-from diffusers import DiffusionPipeline
+from transformers import pipeline
 
 from kserve import (
     Model,
@@ -17,21 +17,22 @@ from kserve import (
 from kserve.errors import InvalidInput
 
 
-class DiffusersModel(Model):
+class AsrModel(Model):
     def __init__(self, name: str):
         super().__init__(name)
-        self.model_id = os.environ.get("MODEL_ID", default="/mnt/models")
+        # self.model_id = os.environ.get("MODEL_ID", default="/mnt/models")
+        self.model_id = os.environ.get("MODEL_ID", default="openai/whisper-small")
+        self.device = (
+            "cuda" if torch.cuda.is_available()
+            else "mps" if torch.backends.mps.is_available()
+            else "cpu"
+        )
         self.pipeline = None
-        self.refiner = None
         self.ready = False
         self.load()
 
     def load(self):
-
-        pipeline = DiffusionPipeline.from_pretrained(self.model_id)
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        pipeline.to(device)
-        self.pipeline = pipeline
+        self.pipeline = pipeline("automatic-speech-recognition", model="openai/whisper-small", device=self.device)
         # The ready flag is used by model ready endpoint for readiness probes,
         # set to True when model is loaded successfully without exceptions.
         self.ready = True
@@ -51,22 +52,13 @@ class DiffusersModel(Model):
     def predict(
             self, payload: Union[Dict, InferRequest], headers: Dict[str, str] = None
     ) -> Union[Dict, InferResponse]:
-        image = self.pipeline(**payload).images[0]
-        image_bytes = io.BytesIO()
-        image.save(image_bytes, format='PNG')
-        image_bytes.seek(0)
-
-        im_b64 = base64.b64encode(image_bytes.read())
+        transcription = self.pipeline(".ignore/hello-world.mp3")
 
         return {
             "predictions": [
                 {
                     "model_name": self.model_id,
-                    "prompt": payload["prompt"],
-                    "image": {
-                        "format": "PNG",
-                        "b64": im_b64
-                    }
+                    "transcription": transcription
                 }
             ]}
 
@@ -75,6 +67,6 @@ parser = argparse.ArgumentParser(parents=[model_server.parser])
 args, _ = parser.parse_known_args()
 
 if __name__ == "__main__":
-    model = DiffusersModel(args.model_name)
+    model = AsrModel(args.model_name)
     model.load()
     ModelServer().start([model])
